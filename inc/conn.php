ea058560c3929c201a4dabac52a8b727b0bbfdd3
ninhvu2605 <79571072+ninhvu2605@users.noreply.github.com>
2021-04-01 13:18:18 +0700

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -1,6 +1,5 @@
 <?php
-$connection_string = "host=ec2-34-225-103-117.compute-1.amazonaws.com port=5432 dbname=dcg72tr1l83dd3 user=eophfavabkrorx password=cada536917ba323da06136f1b0fd36d8eeae9fee194a8d611e249fd04c60fa78 ";
-$dbconn = pg_connect(connection_string);
+$conn = pg_connect("host=ec2-34-225-103-117.compute-1.amazonaws.com port=5432 dbname=dcg72tr1l83dd3 user=eophfavabkrorx password=cada536917ba323da06136f1b0fd36d8eeae9fee194a8d611e249fd04c60fa78");
 if(isset($_POST['username'])){
 	$username = $_POST['username'];
 }
