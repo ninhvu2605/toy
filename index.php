998be2c1ebdfef8c8047d7174e22557c72c5b4cc
ninhvu2605 <79571072+ninhvu2605@users.noreply.github.com>
2021-04-01 16:27:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 include("./inc/conn.php");
 include('login.php');
+<html>
+<head>
+	<title>CHuc mung </title>
+</head>
+<body>
+	<h1>Chuc mung ban da dang nhap thanh cong </h1>
+</body>
+</html>
 ?>
