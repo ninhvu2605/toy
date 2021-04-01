c2c545e45040c65af3123778fc761bd10c116bf6
ninhvu2605 <79571072+ninhvu2605@users.noreply.github.com>
2021-04-01 16:47:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-include(" inc/conn.php");
+<?php
+$conn = pg_connect("host=ec2-34-225-103-117.compute-1.amazonaws.com port=5432 dbname=dcg72tr1l83dd3 user=eophfavabkrorx password=cada536917ba323da06136f1b0fd36d8eeae9fee194a8d611e249fd04c60fa78");
+if(!$conn){echo "Lost Connect";}
 if(isset($_POST['login'])){
 	$username = $_POST['username'];
  	$password = $_POST['password'];
