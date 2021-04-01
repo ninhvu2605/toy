aeae1b65329111b34cd951e5462faf4da9121ea9
ninhvu2605 <79571072+ninhvu2605@users.noreply.github.com>
2021-04-01 15:32:39 +0700

    Update login.php

--- login.php
+++ login.php
@@ -7,8 +7,8 @@ if ($_SERVER['REQUEST_METHOD']=='POST')
 	$password = $_POST['pass'];
 
 	$user = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM user WHERE username='{$username}' AND password = '{$password}'"));
-	if ($username){
-		$_SESSION['username'] = $username['username'];
+	if ($user){
+		$_SESSION['user'] = $user['username'];
 		header ('location:index.php');
 		die;
 	}
@@ -57,7 +57,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST')
 					  	<h1 class="logo "><a href="">LOGO</a></h1>
 					  	<h3 >Sign into your account</h3>
 					  	<div class="form-group" method="POST">
-					  		<input type="username" name="username" class="form-control" placeholder="Username">
+					  		<input type="username" name="name" class="form-control" placeholder="Username">
 					  		<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-lock" viewBox="0 0 16  16">
 							  <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
 							</svg>
